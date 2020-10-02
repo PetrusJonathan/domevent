844cee7a892427273fafe4104ae00bd2b9c761da
PetrusJonathan <petrusjonathan223@gmail.com>
2020-10-02 22:53:51 +0700

    update file saya

--- main.js
+++ main.js
@@ -22,3 +22,23 @@ const hideModal = () => {
     //jadikan dia tersembunyi
     modal.style.display = 'none'
 }
+// masukan gambar di showcase
+//kita ambil dulu elemnt yang kita inginkan
+
+
+let showcase = document.getElementById('wrapper_showcase')
+let nama = 'jonathan'
+//KITA INJECT ELEMENTNYA DENGAN SYNTAX HTML
+// showcase.innerHTML += `
+
+// <img src="https://picsum.photos/seed/24/300"alt="gambar no 24"/>
+
+
+// `
+
+for (var i = 0; i < 42; i++) {
+    showcase.innerHTML += `
+    <img src="https://picsum.photos/seed/${43112 + i}/200"alt='gambar ke ${i}'/>
+    
+    `
+}
